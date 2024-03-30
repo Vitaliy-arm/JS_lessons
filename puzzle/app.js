8afe173c33d890102b8dd372751d1a6da2b1ab83
Vitalii <vitalii.lohoida@corezoid.com>
2024-03-30 21:47:40 +0200

    Add basic logic for puzzle

--- puzzle/app.js
+++ puzzle/app.js
@@ -3,19 +3,30 @@ const riddle = {
     correctAnswer: 'лампочка',
     hints: ['это съдобное', 'это фрукт'],
     tries: 3,
-    checkAnswer() {}
+    checkAnswer(str) {
+        if (str.toLowerCase().includes(this.correctAnswer)){
+            alert('Правильный ответ');
+            console.log('Правильный ответ');
+        } else {
+            alert('Неправильный ответ');
+            console.log('Неправильный ответ');
+        }
+
+    }
 }
 
 window.onload = function() {
     document.getElementById('riddle').innerText = riddle.question;
 }
 
-function chek() {
+function check() {
     const input = document.getElementsByTagName('input')[0];
 
     const guessedAnswer = input.value;
+    console.log(guessedAnswer);
 
     if (guessedAnswer) {
         // вызвать метод checkAnswer() {}
+        riddle.checkAnswer(guessedAnswer);
     }
 }
